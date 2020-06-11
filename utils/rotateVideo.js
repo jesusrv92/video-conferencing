ef79e1aefe3b3e9853c126a82bead171fc3bcf8d
Jesus Romero <jhromero@tekmexico.com>
2020-06-11 12:17:32 -0700

    refactor: removed unnecessary file

--- utils/rotateVideo.js
+++
@@ -1,6 +0,0 @@
-export default function rotateVideo(video) {
-    video.style[navigator.mozGetUserMedia ? 'transform' : '-webkit-transform'] = 'rotate(0deg)';
-    setTimeout(function () {
-        video.style[navigator.mozGetUserMedia ? 'transform' : '-webkit-transform'] = 'rotate(360deg)';
-    }, 1000);
-}
