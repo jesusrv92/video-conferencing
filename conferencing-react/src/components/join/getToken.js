7cfe8b2432516eaa57820418f9138f87af20c9f0
Jesus Romero <jhromero@tekmexico.com>
2020-07-17 15:49:39 -0700

    fix: accessing correct variable

--- conferencing-react/src/components/join/getToken.js
+++ conferencing-react/src/components/join/getToken.js
@@ -37,7 +37,7 @@ function createSession(sessionId) {
             })
             .catch((response) => {
                 var error = Object.assign({}, response);
-                if (error.response.status === 409) {
+                if (response.status === 409) {
                     resolve(sessionId);
                 } else {
                     console.log(error);
