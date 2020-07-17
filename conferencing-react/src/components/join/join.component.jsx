2a8a558c29cfe864a88b1bc2e80ec69a542bcd77
Jesus Romero <jhromero@tekmexico.com>
2020-07-17 14:15:05 -0700

    feat: changed the variables to be obtained from environment.

--- conferencing-react/src/components/join/join.component.jsx
+++ conferencing-react/src/components/join/join.component.jsx
@@ -19,8 +19,8 @@ import { setPage, toggleMic, toggleVideo } from '../../utils/actions';
 import { OpenVidu } from 'openvidu-browser';
 import axios from 'axios';
 import getUserMedia from '../../modules/getUserMedia-async';
-const OPENVIDU_SERVER_URL = 'https://localhost:4443' //'https://34.234.174.140:4443';
-const OPENVIDU_SERVER_SECRET = 'MY_SECRET';
+const OPENVIDU_SERVER_URL = process.env.OPENVIDU_SERVER_URL;
+const OPENVIDU_SERVER_SECRET = process.env.OPENVIDU_SERVER_SECRET;
 
 export default function Join() {
 
@@ -31,68 +31,68 @@ export default function Join() {
   
   const localVideoRef = React.createRef();
 
-  // function createToken(sessionId) {
-  //   return new Promise((resolve, reject) => {
-  //     var data = JSON.stringify({ session: sessionId });
-  //     axios
-  //       .post(OPENVIDU_SERVER_URL + '/api/tokens', data, {
-  //         headers: {
-  //           Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
-  //           'Content-Type': 'application/json',
-  //         },
-  //       })
-  //       .then((response) => {
-  //         console.log('TOKEN', response);
-  //         resolve(response.data.token);
-  //       })
-  //       .catch((error) => reject(error));
-  //   });
-  // }
+  function createToken(sessionId) {
+    return new Promise((resolve, reject) => {
+      var data = JSON.stringify({ session: sessionId });
+      axios
+        .post(OPENVIDU_SERVER_URL + '/api/tokens', data, {
+          headers: {
+            Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          console.log('TOKEN', response);
+          resolve(response.data.token);
+        })
+        .catch((error) => reject(error));
+    });
+  }
 
-  // function createSession(sessionId) {
-  //   return new Promise((resolve, reject) => {
-  //     var data = JSON.stringify({ customSessionId: sessionId });
-  //     axios
-  //       .post(OPENVIDU_SERVER_URL + '/api/sessions', data, {
-  //         headers: {
-  //           Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
-  //           'Content-Type': 'application/json',
-  //         },
-  //       })
-  //       .then((response) => {
-  //         console.log('CREATE SESION', response);
-  //         resolve(response.data.id);
-  //       })
-  //       .catch((response) => {
-  //         var error = Object.assign({}, response);
-  //         if (error.response.status === 409) {
-  //           resolve(sessionId);
-  //         } else {
-  //           console.log(error);
-  //           console.warn(
-  //             'No connection to OpenVidu Server. This may be a certificate error at ' +
-  //             OPENVIDU_SERVER_URL,
-  //           );
-  //           if (
-  //             window.confirm(
-  //               'No connection to OpenVidu Server. This may be a certificate error at "' +
-  //               OPENVIDU_SERVER_URL +
-  //               '"\n\nClick OK to navigate and accept it. ' +
-  //               'If no certificate warning is shown, then check that your OpenVidu Server is up and running at "' +
-  //               OPENVIDU_SERVER_URL +
-  //               '"',
-  //             )
-  //           ) {
-  //             window.location.assign(OPENVIDU_SERVER_URL + '/accept-certificate');
-  //           }
-  //         }
-  //       });
-  //   });
-  // }
+  function createSession(sessionId) {
+    return new Promise((resolve, reject) => {
+      var data = JSON.stringify({ customSessionId: sessionId });
+      axios
+        .post(OPENVIDU_SERVER_URL + '/api/sessions', data, {
+          headers: {
+            Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          console.log('CREATE SESION', response);
+          resolve(response.data.id);
+        })
+        .catch((response) => {
+          var error = Object.assign({}, response);
+          if (error.response.status === 409) {
+            resolve(sessionId);
+          } else {
+            console.log(error);
+            console.warn(
+              'No connection to OpenVidu Server. This may be a certificate error at ' +
+              OPENVIDU_SERVER_URL,
+            );
+            if (
+              window.confirm(
+                'No connection to OpenVidu Server. This may be a certificate error at "' +
+                OPENVIDU_SERVER_URL +
+                '"\n\nClick OK to navigate and accept it. ' +
+                'If no certificate warning is shown, then check that your OpenVidu Server is up and running at "' +
+                OPENVIDU_SERVER_URL +
+                '"',
+              )
+            ) {
+              window.location.assign(OPENVIDU_SERVER_URL + '/accept-certificate');
+            }
+          }
+        });
+    });
+  }
 
-  // function getToken() {
-  //   return createSession(state.mySessionId).then((sessionId) => createToken(sessionId));
-  // }
+  function getToken() {
+    return createSession(state.mySessionId).then((sessionId) => createToken(sessionId));
+  }
 
   // React.useEffect(() => {
   //   const init = async () => {
