7f6509e001a165e1ea2fe4c961934373b3b331fd
Jesus Romero <jhromero@tekmexico.com>
2020-07-24 16:36:04 -0700

    fix: changed from object to individual functions. Now they return the response data instead of the whole response so it is easier to work with it.

--- conferencing-react/src/components/videoCall/recordCall.js
+++ conferencing-react/src/components/videoCall/recordCall.js
@@ -2,40 +2,40 @@ import axios from 'axios'
 
 import { OPENVIDU_SERVER_SECRET, OPENVIDU_SERVER_URL } from '../../utils/openViduConfig'
 
-export default {
-    async recordCall(sessionId) {
-        var data = JSON.stringify({ session: sessionId });
-        try {
-            let response = await axios.post(OPENVIDU_SERVER_URL + '/api/recordings/start', data, {
-                headers: {
-                    Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
-                    'Content-Type': 'application/json',
-                },
-            });
-            console.log('Recording information', response);
-            return response;
-        }
-        catch (e) {
-            console.log(`Unable to start recording this session.`, e)
-            return e;
-        }
-    },
-    async stopRecording(recordingId) {
-        var data = JSON.stringify({});
-        try {
-            let response = await axios.post(OPENVIDU_SERVER_URL + '/api/recordings/stop/' + recordingId, data, {
-                headers: {
-                    Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
-                    'Content-Type': 'application/json',
-                },
-            });
-            console.log('Succesfully stopped recording the session with recording ID ', recordingId)
-            return response;
-        }
-        catch (e) {
-            console.log(`Unable to stop the recording with recording ID`, recordingId, e)
-            console.log(e);
-            return e;
-        }
+export async function recordCall(sessionId) {
+    var data = JSON.stringify({ session: sessionId, outputMode: "INDIVIDUAL" });
+    try {
+        let response = await axios.post(OPENVIDU_SERVER_URL + '/api/recordings/start', data, {
+            headers: {
+                Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log('Recording information', response);
+        return response.data;
+    }
+    catch (e) {
+        const error = Object.assign({}, e)
+        console.log(`Unable to start recording this session.`, error)
+        return e;
+    }
+}
+export async function stopRecording(recordingId) {
+    var data = JSON.stringify({});
+    try {
+        let response = await axios.post(OPENVIDU_SERVER_URL + '/api/recordings/stop/' + recordingId, data, {
+            headers: {
+                Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + OPENVIDU_SERVER_SECRET),
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log('Succesfully stopped recording the session with recording ID ', recordingId)
+        return response.data;
+    }
+    catch (e) {
+        const error = Object.assign({}, e)
+        console.log(`Unable to stop the recording with recording ID`, recordingId, error)
+        console.log(e);
+        return e;
     }
 }
