778a0b8e21c62828ed20904a92fecd8e4bfc9c6a
Jesus Romero <jhromero@tekmexico.com>
2020-07-24 12:31:20 -0700

    feat: adding remove user functionality (WIP)

---
+++ conferencing-react/src/components/video/removeUser.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+import { OPENVIDU_SERVER_SECRET, OPENVIDU_SERVER_URL } from '../../utils/openViduConfig'
+
+export default function removeUser(user) {
+    // TODO: EMIT SIGNAL TO USER TO LEAVE SESSION
+}
