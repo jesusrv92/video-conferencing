a7411825fe55505f7a5135c2be325d2394e21615
Jesus Romero <jhromero@tekmexico.com>
2020-07-24 12:31:08 -0700

    feat: adding recording call functionality (WIP)

---
+++ conferencing-react/src/components/videoCall/recordCall.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+import { OPENVIDU_SERVER_SECRET, OPENVIDU_SERVER_URL } from '../../utils/openViduConfig'
+
+export default function recordCall() {
+    // TODO: EMIT SIGNAL TO START RECORDING SESSION
+}
