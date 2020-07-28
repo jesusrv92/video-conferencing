e5d386158fa903b9d8c646457d6b520db0df6c65
Jesus Romero <jhromero@tekmexico.com>
2020-07-28 14:26:50 -0700

    feat: changed framerate 15 to reduce bandwidth

--- conferencing-react/src/components/join/join.component.jsx
+++ conferencing-react/src/components/join/join.component.jsx
@@ -112,7 +112,7 @@ export default function Join() {
           publishAudio: true,     // Whether you want to start publishing with your audio unmuted or not
           publishVideo: true,     // Whether you want to start publishing with your video enabled or not
           resolution: '640x480',  // The resolution of your video
-          frameRate: 30,          // The frame rate of your video
+          frameRate: 15,          // The frame rate of your video
           insertMode: 'APPEND',   // How the video is inserted in the target element 'video-container'
           mirror: false           // Whether to mirror your local video or not
         });
