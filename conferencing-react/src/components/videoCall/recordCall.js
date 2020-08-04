3186c4ec44d30baca3506dc643f8c8b12e7d2c1b
Jesus Romero <jhromero@tekmexico.com>
2020-08-04 12:56:41 -0700

    docs: explaining what the function does

--- conferencing-react/src/components/videoCall/recordCall.js
+++ conferencing-react/src/components/videoCall/recordCall.js
@@ -1,3 +1,13 @@
+/*
+These functions will use the OpenVidu Server API to start and stop the recording of the sessions
+
+recordCall will take a sessionId to start recording the call. The function will return a recording ID that
+will be needed to stop the recording of the call. You can modify the parameters of the recording in the data object.
+For more info about the parameters: https://docs.openvidu.io/en/2.15.0/reference-docs/REST-API/#post-apirecordingsstart
+
+stopRecording takes the recordingId that is returned by recordCall and sends the signal to the server to stop
+the recording of the session.
+*/
 import axios from 'axios'
 
 import { OPENVIDU_SERVER_SECRET, OPENVIDU_SERVER_URL } from '../../utils/openViduConfig'
