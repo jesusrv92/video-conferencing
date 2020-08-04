9507df1f894fc34185bbbb436ec60d0ff66baecd
Jesus Romero <jhromero@tekmexico.com>
2020-08-03 17:28:32 -0700

    docs: added function explanation

--- conferencing-react/src/utils/removeUser.js
+++ conferencing-react/src/utils/removeUser.js
@@ -1,3 +1,13 @@
+/* 
+Sends signal to user to remove it. This is done by sending a 
+removed signal using the OpenVidu Session API. Once the target
+user receives the removed signal, it will run the leaveSession
+function found in the OpenVidu Session API.
+
+Beware, if the user isn't valid (meaning that it doesn't contain a stream object property
+that has a connection property) it will send the signal to all users and all users will
+leave the call.
+*/
 export default async function removeUser(session, user) {
     try {
         await session.signal({
