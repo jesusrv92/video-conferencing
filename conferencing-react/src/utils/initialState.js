115c6b67f8d6c42d1f6db07010bb976342853e43
Jesus Romero <jhromero@tekmexico.com>
2020-08-13 17:20:16 -0700

    refactor: moving openvidu properties to root state

--- conferencing-react/src/utils/initialState.js
+++ conferencing-react/src/utils/initialState.js
@@ -9,11 +9,9 @@ export default {
   optionsMenu: false, // Display options menu in videocall component
   sidebar: false, // Display sidebar menu in videocall component
   users: [], // Current users in call
-  openVidu: {
-    mySessionID: `${shortid.generate()}${shortid.generate()}`, // Session you're going to join
-    myUserName: 'Participant' + Math.floor(Math.random() * 100), // The username that you're going to use
-    session: undefined, // OpenVidu Session API object
-    publisher: undefined, // OpenVidu Publisher API object
-    recordingID: '', // keeps track of the ID if the call is being recorded.
-  }
+  mySessionID: `${shortid.generate()}${shortid.generate()}`, // Session you're going to join
+  myUserName: 'Participant' + Math.floor(Math.random() * 100), // The username that you're going to use
+  session: undefined, // OpenVidu Session API object
+  publisher: undefined, // OpenVidu Publisher API object
+  recordingID: '', // keeps track of the ID if the call is being recorded.
 }
