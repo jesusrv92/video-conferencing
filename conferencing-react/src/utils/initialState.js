a948fb36be1ae408de0af6fb9ca23fe6e3a0de7c
Jesus Romero <jhromero@tekmexico.com>
2020-08-17 16:58:30 -0700

    docs: adding description of users property

--- conferencing-react/src/utils/initialState.js
+++ conferencing-react/src/utils/initialState.js
@@ -8,7 +8,7 @@ export default {
   detailsMenu: false, // Display details menu in videocall component
   optionsMenu: false, // Display options menu in videocall component
   sidebar: false, // Display sidebar menu in videocall component
-  users: [], // Current users in call
+  users: [], // Current users in call, this is an array of objects with remoteClientId and peerConnection (RTCPeerConnection) as properties
   meetingCode: `${shortid.generate()}${shortid.generate()}`, // Session you're going to join
   myUserName: 'Participant' + Math.floor(Math.random() * 100), // The username that you're going to use
   session: undefined, // OpenVidu Session API object
